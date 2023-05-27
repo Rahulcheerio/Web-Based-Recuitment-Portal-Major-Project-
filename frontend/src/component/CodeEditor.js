a4cb4562f6af0d75dfcb8603ea0edc0dceb8b861
Rahulcheerio <rahul@cheerio.in>
2023-05-27 10:10:02 +0530

    first commit

---
+++ frontend/src/component/CodeEditor.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link} from 'react-router-dom';
+
+const CodeEditor = () => {
+  return (
+    <a href="https://realtime-code-editor-using-socket.netlify.app/" target="_blank" rel="noopener noreferrer">Go to Code Editor</a>
+  )
+}
+
+export default CodeEditor
---
+++ frontend/src/component/Navbar.js
@@ -0,0 +1,103 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { Link} from 'react-router-dom';
+import isAuth, { userType } from "../lib/isAuth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Navbar = (props) => {
+  const classes = useStyles();
+  let history = useHistory();
+
+  const handleClick = (location) => {
+    console.log(location);
+    history.push(location);
+  };
+
+  const handleClickOpen = () => {
+    window.open( `https://realtime-sharing-code-editor.netlify.app/`);
+  }
+
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          Job Portal
+        </Typography>
+        <Button color="inherit" onClick={handleClickOpen}>
+           Code-Editor
+        </Button>
+        {isAuth() ? (
+          userType() === "recruiter" ? (
+            <>
+              <Button color="inherit" onClick={() => handleClick("/home")}>
+                Home
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/addjob")}>
+                Add Jobs
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/myjobs")}>
+                My Jobs
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/employees")}>
+                Employees
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/profile")}>
+                Profile
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/logout")}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" onClick={() => handleClick("/home")}>
+                Home
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() => handleClick("/applications")}
+              >
+                Applications
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/profile")}>
+                Profile
+              </Button>
+              <Button color="inherit" onClick={() => handleClick("/logout")}>
+                Logout
+              </Button>
+            </>
+          )
+        ) : (
+          <>
+            <Button color="inherit" onClick={() => handleClick("/login")}>
+              Login
+            </Button>
+            <Button color="inherit" onClick={() => handleClick("/signup")}>
+              Signup
+            </Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
